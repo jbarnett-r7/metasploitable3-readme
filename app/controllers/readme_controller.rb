78ea31e8813f536ea2a0bf8745b8141b17ec2909
vagrant <vagrant@metasploitableUB>
2017-04-19 20:10:39 +0000

    Add action for readme_controller.rb

--- app/controllers/readme_controller.rb
+++ app/controllers/readme_controller.rb
@@ -1,2 +1,9 @@
 class ReadmeController < ApplicationController
+  
+  def index
+    puts "params[:os] is #{params[:os]}" if params[:os]
+    template = 'default'
+    template = params[:os] if params[:os]
+    render template
+  end
 end
